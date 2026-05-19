b8b458b4590493bf211f71141f7603475a1ae364
Md. Amanullah Islam <amanullah435islam@gmail.com>
2026-05-19 12:21:57 +0600

    finally check the fingureprint by regiser & login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -16,6 +16,12 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+
+  {
+    path: 'login',
     loadComponent: () =>
       import('./auth/login/login')
         .then(m => m.LoginComponent)
